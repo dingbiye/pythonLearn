636b162a3538d924b8c24324f6e674745615701f
fubing <fubing>
2019-01-15 08:16:10 +0800

    a

---
+++ pythonFirst/sorting.py
@@ -0,0 +1,20 @@
+data = [5, 3, 2, 43, 32]
+
+print(data)
+
+#data.sort()
+
+print(data)
+
+data2 = sorted(data)
+
+print(data2)
+
+def addOne(x):
+    return x + 1
+
+list = [1, 2, 4, 8]
+
+print(list)
+
+print([addOne(x) for x in list])
